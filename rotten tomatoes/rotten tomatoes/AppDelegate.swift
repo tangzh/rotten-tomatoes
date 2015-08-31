f777d1f942d570977b866dc3fab5e5aed561f4a9
Tang <tangz@tumblr.com>
2015-08-31 00:52:54 -0700

    change nav bar appearance

--- rotten tomatoes/rotten tomatoes/AppDelegate.swift
+++ rotten tomatoes/rotten tomatoes/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().barTintColor = UIColor.redColor()
+        UINavigationBar.appearance().tintColor = UIColor.whiteColor()
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.orangeColor()]
+        
         return true
     }
 
